1af115071aa22c259f4142c61fff4872d60bab77
paulboul1013 <paulboul1013@gmail.com>
2025-11-12 11:08:16 +0800

    Add interpreter test & fix x86 compiler test file path

--- bf_test.sh
+++ bf_test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#test interpreter
+gcc -I. -o interpreter_main interpreter/main.c
+time ./interpreter_main mandelbrot.b
+
+
 # test compiler_x86
-gcc -o compiler_x86 compiler_x86.c
+gcc -I. -o  compiler_x86 compiler_x86_source/compiler_x86.c
 ./compiler_x86 mandelbrot.b > mandelbrot.s
 gcc -m32 -nostdlib -no-pie -o mandelbrot mandelbrot.s
 time ./mandelbrot
 
 # test compiler_x86_64
-gcc -o compiler_x86_64 compiler_x86_64.c
+gcc -I. -o  compiler_x86_64 compiler_x86_source/compiler_x86_64.c
 ./compiler_x86_64 mandelbrot.b > mandelbrot.s
 gcc -nostdlib -no-pie -o mandelbrot mandelbrot.s
 time ./mandelbrot
