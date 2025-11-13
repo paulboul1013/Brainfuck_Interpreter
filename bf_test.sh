c3d3ffb1485cb74bf85d306256020a99862d3574
paulboul1013 <paulboul1013@gmail.com>
2025-11-13 10:41:19 +0800

    Add llvm test script

--- bf_test.sh
+++ bf_test.sh
@@ -2,17 +2,23 @@
 
 #test interpreter
 gcc -I. -o interpreter_main interpreter/main.c
-time ./interpreter_main mandelbrot.b
+time ./interpreter_main mandelbrot.bf
 
 
 # test compiler_x86
 gcc -I. -o  compiler_x86 compiler_x86_source/compiler_x86.c
-./compiler_x86 mandelbrot.b > mandelbrot.s
+./compiler_x86 mandelbrot.bf > mandelbrot.s
 gcc -m32 -nostdlib -no-pie -o mandelbrot mandelbrot.s
 time ./mandelbrot
 
 # test compiler_x86_64
 gcc -I. -o  compiler_x86_64 compiler_x86_source/compiler_x86_64.c
-./compiler_x86_64 mandelbrot.b > mandelbrot.s
+./compiler_x86_64 mandelbrot.bf > mandelbrot.s
 gcc -nostdlib -no-pie -o mandelbrot mandelbrot.s
+time ./mandelbrot
+
+test compiler_llvm
+gcc -I. -o  compiler_llvm llvm/llvm.c
+./compiler_llvm mandelbrot.bf > mandelbrot.ll
+clang -o mandelbrot mandelbrot.ll
 time ./mandelbrot
